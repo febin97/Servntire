7cff4a4d624b9f8b68ef5515593e4528ff630648
febin97 <31316987+febin97@users.noreply.github.com>
2018-05-17 13:01:21 +0530

    Add files via upload

---
+++ Reverse_linkedlist.py
@@ -0,0 +1,38 @@
+#Reverse a Linked List
+
+class Node :
+    def __init__(self, data) :
+        self.data = data
+        self.next = None
+
+def reverseLL(ls):
+    if ls == None:
+        return None
+    if ls.next == None:
+        return ls
+    rest = reverseLL(ls.next)
+    ls.next.next = ls
+    ls.next = None
+    return rest
+
+
+arr = [int(n) for n in input("Enter the elements of the Linked List\n").split(",")]
+ls = Node(arr[0])
+head  = ls
+ls = Node(arr[1])
+head.next = ls
+for i in range(2,len(arr)):
+    ls.next = Node(arr[i])
+    ls = ls.next
+ls = head
+while ls:
+    print(ls.data,end="->")
+    ls = ls.next
+print()
+
+
+ls = reverseLL(head)
+
+while ls:
+    print(ls.data,end="->")
+    ls = ls.next
